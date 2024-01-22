9e0d5e6f991a01537095b938550b71f262a11182
Bishu <bishu.tamang@inlognetwork.com>
2024-01-22 12:09:47 +0530

    code for models relation product, user, order et

---
+++ zotravo/models.py
@@ -0,0 +1,53 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=200,null=True)
+    price= models.FloatField()
+    CHOICES = [
+        ('bed_wise', 'Rate Per Bed'),
+        ('room', 'Rate Per Room'),
+    ]
+    room_type = models.CharField(
+        max_length=20,
+        choices=CHOICES,
+        default='bed_wise',  # Set a default value if needed
+    )
+
+    def __str__(self):
+        return f"{self.room_type} - {self.id}"
+
+
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product, related_name='images', on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='product_images/')
+
+    def __str__(self):
+        return f"Image for {self.product.name}"
+
+
+class CustomUser(AbstractUser):
+    # Add your custom fields here
+    # partner_id = models.ForeignKey(Partner)
+    customer = models.BooleanField(default=False, null=True, blank=False)
+    publisher = models.BooleanField(default=False, null=True, blank=False)
+    profile_picture = models.ImageField(upload_to='profile_pics/', null=True, blank=True)
+
+
+class Partner(models.Model):
+    user_id = models.ForeignKey(CustomUser)
+
+
+class Orders(models.Model):
+    customer = models.ForeignKey(Partner)
+    name = models.CharField(max_length=200,null=True)
+    email = models.CharField(max_length=200, null=True)
+    create_date = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+
